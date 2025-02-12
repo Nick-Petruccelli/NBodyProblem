3fbc1539fd544b6f6c60be1e040a631bafb5d97f
NickPetruccelli <NickPetro18@gmail.com>
2025-02-12 13:29:12 -0500

    added option to name output file

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@ int main(int argc, char* argv[]) {
   unsigned int dump_rate;
   double dt;
   bool initialized = false;
+  char* out_file;
   if(argc == 1){
     std::cout << "hit default" << std::endl;
     sim.file_init("./sim_input/solar.tsv");
@@ -18,36 +19,40 @@ int main(int argc, char* argv[]) {
     dt = 200.0;
     dump_rate = 200;
     initialized = true;
+    out_file = (char*)"./out/sim_data.tsv";
   }else{
     char* type = argv[1];
     if(strncmp(type, "rand", 4) == 0){
       std::cout << "hit rand" << std::endl;
-      assert(argc == 6);
+      assert(argc == 7);
       sim.rand_init(atoi(argv[2]));
       n_iters = atoi(argv[3]);
       dt = std::stod(argv[4]);
       dump_rate = atoi(argv[5]);
+      out_file = argv[6];
       initialized = true;
     }else if(strncmp(type, "file", 4) == 0){
       std::cout << "hit file" << std::endl;
-      assert(argc == 6);
+      assert(argc == 7);
       sim.file_init(argv[2]);
       n_iters = atoi(argv[3]);
       dt = std::stod(argv[4]);
       dump_rate = atoi(argv[5]);
+      out_file = argv[6];
       initialized = true;
     }else if(strncmp(type, "three_body", 4) == 0){
       std::cout << "hit three body" << std::endl;
-      assert(argc == 5);
+      assert(argc == 6);
       sim.three_body_init();
       n_iters = atoi(argv[2]);
       dt = std::stod(argv[3]);
       dump_rate = atoi(argv[4]);
+      out_file = argv[5];
       initialized = true;
     }
   }
 
   assert(initialized);
-  sim.start(dt, n_iters, dump_rate, "./out/sim_data.tsv");
+  sim.start(dt, n_iters, dump_rate, out_file);
   sim.close();
 }
